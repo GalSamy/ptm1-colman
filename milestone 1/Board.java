c4ba02a6c63b3f113870836391fab9405558ae5b
galsamy <galsamytl@gmail.com>
2022-11-07 23:09:10 +0200

    added word class, started to work on Board 🎲

--- milestone 1/Board.java
+++ milestone 1/Board.java
@@ -1,6 +1,15 @@
 package test;
-
+import test.Word;
 
 public class Board {
+    Word[] words;
+    private static Board b = null;
+    private Board(){
 
+    }
+    public static  Board getBoard(){
+        if(b == null)
+            b = new Board();
+        return b;
+    }
 }
