f60091a54034f70003077e105f7595263ab02bb2
GalSamy <59654405+GalSamy@users.noreply.github.com>
2023-01-22 22:43:42 +0200

    changed MainTrain names. started working on milestone 3.

---
+++ milestone 3/BookScrabbleHandler.java
@@ -0,0 +1,12 @@
+package test;
+import test.DictionaryManager;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class BookScrabbleHandler {
+ private DictionaryManager DM = new DictionaryManager();
+ public void handleClient(InputStream inFromclient, OutputStream outToClient){
+     
+ }
+}
